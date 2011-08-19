7b29cf2e277185a1539f9ddd49c0a43f9cc8555c
Joe Toscano <joe@tigertext.com>
2011-08-19 16:03:42 -0700

    Initial Commit

---
+++ LookIO/LookIO.m
@@ -0,0 +1,23 @@
+//
+//  LookIO.m
+//  LookIO
+//
+//  Created by Joseph Toscano on 8/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LookIO.h"
+
+@implementation LookIO
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
