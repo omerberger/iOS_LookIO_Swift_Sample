1c11b80960bc45dea481fbeece347d5f542091e5
marc <marc@mc85472935.local>
2012-03-19 20:19:25 -0400

    Added Jenkins build script.

---
+++ go_jenkins_go.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd /Users/marc/Development/ios_lib
+git checkout .
+git pull origin master
+./build_lio_41.sh $1
+
+if [ $? -ne 0 ]
+then
+    echo Sorry, Jenkins, but the build failed.
+    rm -rf _LOOKIO_$1_
+    exit 1
+fi
+
+cp -f _LOOKIO_$1_/ship_it/* ../LookIO-Libraries/iOS/release
+rm -rf _LOOKIO_$1_
+cd ../LookIO-Libraries
+git commit -a -m "Jenkins build v$1"
+git push origin master
