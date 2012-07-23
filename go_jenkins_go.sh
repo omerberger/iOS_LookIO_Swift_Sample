2e93aa842f761c9ad054e7d2eaa57e23582eac4b
marc <marc@mc85472935.local>
2012-07-23 18:46:28 -0400

    Debugging.

--- go_jenkins_go.sh
+++ go_jenkins_go.sh
@@ -22,7 +22,9 @@ fi
 
 # Publish!
 pushd ../LookIO-Libraries
+echo "git checkout ."
 git checkout .
+echo "git checkout dev"
 git checkout dev
 rm -rf iOS/release/*
 cp -v -f -R  ../ios_lib/_LOOKIO_$1_/libLookIO.a ../ios_lib/_LOOKIO_$1_/LookIO.bundle ../ios_lib/_LOOKIO_$1_/LIOLookIOManager.h iOS/release
