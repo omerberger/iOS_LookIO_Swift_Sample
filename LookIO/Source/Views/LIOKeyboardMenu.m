71171529640aca974fc20bd15661765a18fc916c
Yaron Karasik <yaronkarasik@gmail.com>
2013-08-12 10:26:58 -0400

    Change keyboard background color

--- LookIO/Source/Views/LIOKeyboardMenu.m
+++ LookIO/Source/Views/LIOKeyboardMenu.m
@@ -19,7 +19,7 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.55];
+        self.backgroundColor = [UIColor colorWithWhite:0.3 alpha:0.55];
         
         buttonsArray = [[NSMutableArray alloc] init];
         
