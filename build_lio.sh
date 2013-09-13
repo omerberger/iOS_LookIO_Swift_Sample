435caed2725ce578caf1abfc3ccb9abaed40b9a7
Yaron Karasik <yaronkarasik@gmail.com>
2013-09-13 20:03:26 +0300

    Update build script to build only modern version of the library

--- build_lio.sh
+++ build_lio.sh
@@ -10,8 +10,6 @@ fi
 
 TARGET_DIR=_LOOKIO_$1_
 LOG_FILE_MAIN=$TARGET_DIR/main_lio_$1.log
-LOG_FILE_LEGACY=$TARGET_DIR/legacy_lio_$1.log
-LOG_FILE_LIPO=$TARGET_DIR/lipo_lio_$1.log
 
 rm -rf $TARGET_DIR
 mkdir $TARGET_DIR
@@ -20,10 +18,6 @@ echo "Building LookIO v$1 (Release), please wait..."
 
 sed -i "" "s/##UNKNOWN_VERSION##/$1/g" ./LookIO/Source/Managers/LIOLookIOManager.h
 
-
-#
-# First, build the "main" library: modern SDK, armv7 armv7s i386.
-#
 rm -rf LookIO/build
 PATH=/Applications/Xcode.app/Contents/Developer/usr/bin:$PATH xcodebuild -project LookIO/LookIO.xcodeproj -target LookIO -configuration Release -xcconfig main.xcconfig &>$LOG_FILE_MAIN
 
@@ -37,42 +31,8 @@ fi
 mkdir -p $TARGET_DIR/LookIO.bundle
 cp LookIO/Resources/Images/LIO* $TARGET_DIR/LookIO.bundle
 cp -R LookIO/Resources/Strings/* $TARGET_DIR/LookIO.bundle
-mv LookIO/build/Release-universal/libLookIO.a $TARGET_DIR/libLookIO_main.a
+cp LookIO/build/Release-universal/libLookIO.a $TARGET_DIR
 cp LookIO/Source/Managers/LIOLookIOManager.h $TARGET_DIR
-
-
-#
-# Next, build the "legacy" library: older SDK, armv6.
-#
-rm -rf LookIO/build
-PATH=/Applications/Xcode4.3.3.app/Contents/Developer/usr/bin:$PATH xcodebuild -project LookIO/LookIO.xcodeproj -target LookIO -configuration Release -xcconfig legacy.xcconfig &>$LOG_FILE_LEGACY
-
-if [ $? -ne 0 ]
-then
-    echo "Legacy build failed. Here's why:"
-    cat "$LOG_FILE_LEGACY"
-    exit 1
-fi
-
-lipo LookIO/build/Release-iphoneos/libLookIO.a -thin armv6 -output $TARGET_DIR/libLookIO_legacy.a
-
-
-#
-# Now, combine the two build products.
-#
-lipo $TARGET_DIR/libLookIO_main.a $TARGET_DIR/libLookIO_legacy.a -create -output $TARGET_DIR/libLookIO.a &>$LOG_FILE_LIPO
-
-if [ $? -ne 0 ]
-then
-    echo "lipo operation failed. Here's why:"
-    cat "$LOG_FILE_LIPO"
-    exit 1
-fi
-
-rm $TARGET_DIR/libLookIO_main.a
-rm $TARGET_DIR/libLookIO_legacy.a
-
 
 # Undo the sed change.
 git checkout ./LookIO/Source/Managers/LIOLookIOManager.h
-
