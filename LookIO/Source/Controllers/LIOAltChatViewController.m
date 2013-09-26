45e9298e94b16993075bdeb7b29f81d4a74cc658
Yaron Karasik <yaronkarasik@gmail.com>
2013-09-26 14:17:35 -0400

    Added delay in presenting attachment upload alert view, to avoid rotation problems.

--- LookIO/Source/Controllers/LIOAltChatViewController.m
+++ LookIO/Source/Controllers/LIOAltChatViewController.m
@@ -2107,7 +2107,16 @@
             resizedImageSize.height = targetSize*(image.size.height/image.size.width);
         }
         pendingImageAttachment = [[[LIOMediaManager sharedInstance] scaleImage:image toSize:resizedImageSize] retain];
-        [self showAttachmentUploadConfirmation];
+        
+        if (padUI)
+            [self showAttachmentUploadConfirmation];
+        else {
+            double delayInSeconds = 1.0;
+            dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+            dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+                [self showAttachmentUploadConfirmation];
+            });
+        }
     }
 }
 
