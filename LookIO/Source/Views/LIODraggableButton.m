6844db71d4441c559168f3d1f3b8aa716e905ce1
Yaron Karasik <yaronkarasik@gmail.com>
2014-03-14 13:34:53 -0700

    Allow tapping the popup message of the button

--- LookIO/Source/Views/LIODraggableButton.m
+++ LookIO/Source/Views/LIODraggableButton.m
@@ -934,5 +934,21 @@
     }
 }
 
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
+{
+    if (!self.clipsToBounds && !self.hidden && self.alpha > 0) {
+        for (UIView *subview in self.subviews.reverseObjectEnumerator) {
+            CGPoint subPoint = [subview convertPoint:point fromView:self];
+            UIView *result = [subview hitTest:subPoint withEvent:event];
+            if (result != nil) {
+                return result;
+                break;
+            }
+        }
+    }
+    
+    // use this to pass the 'touch' onward in case no subviews trigger the touch
+    return [super hitTest:point withEvent:event];
+}
 
 @end
